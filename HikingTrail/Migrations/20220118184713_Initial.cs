ef3ea1e3c38ddbcd94a3cfc471f7faea782a8c45
Katie Pundt <kzpundt@gmail.com>
2022-01-18 10:48:16 -0800

    add Initial migration
    Co-Authored-by: Kim Brannian <brannian@gmail.com>
    Co-authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>

---
+++ HikingTrail/Migrations/20220118184713_Initial.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HikingTrail.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Trails",
+                columns: table => new
+                {
+                    TrailId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Length = table.Column<double>(type: "double", nullable: false),
+                    Configuration = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    StartingElevation = table.Column<int>(type: "int", nullable: false),
+                    ElevationGain = table.Column<int>(type: "int", nullable: false),
+                    Difficulty = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    FamilyFriendly = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    DistanceFromPdx = table.Column<double>(type: "double", nullable: false),
+                    Latitude = table.Column<double>(type: "double", nullable: false),
+                    Longitude = table.Column<double>(type: "double", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trails", x => x.TrailId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Trails");
+        }
+    }
+}
