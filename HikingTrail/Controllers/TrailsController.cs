9447a24a4cfa3a0ceacd2a94435c97384cee136d
Katie Pundt <kzpundt@gmail.com>
2022-01-18 11:27:16 -0800

    add TrailsController with GET methods
    Co-Authored-by: Kim Brannian <brannian@gmail.com>
    Co-authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>

---
+++ HikingTrail/Controllers/TrailsController.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HikingTrail.Models;
+
+namespace HikingTrail.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class TrailsController : ControllerBase
+  {
+    private readonly HikingTrailContext _db;
+    public TrailsController(HikingTrailContext db)
+    {
+      _db = db;
+    }
+
+    // GET: api/trails
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Trail>>>Get(string name, string difficulty, double length, string familyFriendly, double distanceFromPdx)
+    {
+      var query = _db.Trails.AsQueryable();
+
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+      
+      if (difficulty != null)
+      {
+        query = query.Where(entry => entry.Difficulty == difficulty);
+      }
+
+      if (length != 0.0)
+      {
+        query = query.Where(entry => entry.Length == length);
+      }
+
+      if (familyFriendly != null)
+      {
+        query = query.Where(entry => entry.FamilyFriendly == familyFriendly);
+      }
+
+      if (distanceFromPdx != 0.0)
+      {
+        query = query.Where(entry => entry.DistanceFromPdx == distanceFromPdx);
+      }
+
+      return await _db.Trails.ToListAsync();
+    }
+
+    // GET: api/trails/2
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Trail>>GetTrail(int id)
+    {
+      var trail = await _db.Trails.FindAsync(id);
+
+      if (trail == null)
+      {
+        return NotFound();
+      }
+
+      return trail;
+    }
+
+    // [HttpPost]
+    // public async Task<ActionResult<Trail>> Post(Trail trail)
+    // {
+    //   _db.Trails.Add(trail);
+    //   await _db.SaveChangesAsync();
+
+    //   return CreatedAtAction(nameof(GetTrail), new { id = trail.TrailId }, trail);
+    // }
+
+  }
+
+}
